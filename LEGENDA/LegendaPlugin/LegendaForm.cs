8a76a6583a7249c54cc9f14fc3c009b150ae1831
AlicjaZiebinska <alicjaziebinska95@gmail.com>
2025-11-06 22:56:45 +0100

    Fixing Custom DateTimePicker

--- LEGENDA/LegendaPlugin/LegendaForm.cs
+++ LEGENDA/LegendaPlugin/LegendaForm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using LegendPlugin;
 using System.Windows.Forms;
 
 // Komentarz: >>> Aliasujemy typy koloru, aby uniknąć konfliktu nazw.
@@ -16,7 +17,7 @@ namespace LegendPlugin
         // Komentarz: Kontrolki.
         private CheckedListBox clbLayers;
         private TextBox tbJednostka, tbInwestor, tbObiekt, tbTytul, tbProjektant, tbSprawdzajacy, tbOpracowujacy, tbSkala, tbNrRys;
-        DateTimePicker dtpData;
+        private LegendPlugin. CustomDatePicker dtpData;
         private Button btnOk, btnCancel;
 
         // Komentarz: Warstwy wejściowe (z kolorem AutoCAD).
@@ -60,25 +61,22 @@ namespace LegendPlugin
             TextBox T(int y, bool wide = true) => new TextBox { Left = 10, Top = y, Width = wide ? 400 : 180 };
 
             int y0 = 25;
-            grpMeta.Controls.Add(L("TYTUŁ RYSUNKU:", y0)); tbTytul = T(y0 + 18); grpMeta.Controls.Add(tbTytul);
+            grpMeta.Controls.Add(L("TYTUŁ RYSUNKU:", y0)); tbTytul = T(y0 + 25); grpMeta.Controls.Add(tbTytul);
             grpMeta.Controls.Add(L("JEDNOSTKA PROJEKTOWA:", y0 + 50)); tbJednostka = T(y0 + 78); grpMeta.Controls.Add(tbJednostka);
             grpMeta.Controls.Add(L("INWESTOR:", y0 + 110)); tbInwestor = T(y0 + 138); grpMeta.Controls.Add(tbInwestor);
             grpMeta.Controls.Add(L("NAZWA I ADRES OBIEKTU:", y0 + 170)); tbObiekt = T(y0 + 198); grpMeta.Controls.Add(tbObiekt);
             grpMeta.Controls.Add(L("PROJEKTANT:", y0 + 230)); tbProjektant = T(y0 + 258); grpMeta.Controls.Add(tbProjektant);
             grpMeta.Controls.Add(L("SPRAWDZAJĄCY:", y0 + 290)); tbSprawdzajacy = T(y0 + 318); grpMeta.Controls.Add(tbSprawdzajacy);
             grpMeta.Controls.Add(L("OPRACOWAŁ(A):", y0 + 350)); tbOpracowujacy = T(y0 + 378); grpMeta.Controls.Add(tbOpracowujacy);
-            grpMeta.Controls.Add(L("DATA:", y0 + 410)); 
-            var dtpData = new DateTimePicker
+            grpMeta.Controls.Add(L("DATA:", y0 + 410));
+            dtpData = new CustomDatePicker
             {
                 Left = 10,
                 Top = y0 + 438,
-                Width = 180,
-                Format = DateTimePickerFormat.Short,  // format: dd.MM.yyyy
-                Font = new Font("Segoe UI", 9F, FontStyle.Regular)
+                Width = 180
             };
             grpMeta.Controls.Add(dtpData);
-            grpMeta.Controls.Add(L("SKALA:", y0 + 410)); tbSkala = new TextBox { Left = 210, Top = y0 + 438, Width = 90 }; grpMeta.Controls.Add(tbSkala);
-            grpMeta.Controls.Add(L("NR RYS.:", y0 + 410)); tbNrRys = new TextBox { Left = 310, Top = y0 + 438, Width = 100 }; grpMeta.Controls.Add(tbNrRys);
+
 
             btnOk = new Button { Text = "OK", Left = 640, Top = 540, Width = 110, DialogResult = DialogResult.OK };
             btnCancel = new Button { Text = "Anuluj", Left = 760, Top = 540, Width = 110, DialogResult = DialogResult.Cancel };
